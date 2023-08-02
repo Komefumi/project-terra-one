a9a6b6fb106c63676eadf9674f9a37749e9f3baa
Hafiz <komefumi@protonmail.ch>
2023-08-02 18:26:53 +0530

    Adds more fields and custom validation

--- index.php
+++ index.php
@@ -7,33 +7,72 @@
   Author URI: komefumi.github.io
 */
 
-
 class Vecktor_TerraOnePlugin
 {
   private string $setting_prefix = "terraone__";
+  private string $setting_error_suffix = "__error";
   private string $setting_section_default = 'terraone__first_section';
   private string $setting_slug = 'word-count-settings';
   private string $setting_group = 'word-count-plugin';
   private array $setting_base_names = [
     'location',
-    'headline'
+    'headline',
+    'word_count',
+    'character_count',
+    'read_time',
   ];
   private array $setting_name_to_data = array(
-    'location' => array(
-      'default_val' => 0,
+    "location" => array(
+      'default_val' => '0',
       'display_name' => 'Display Location',
       'display_fn' => 'location_html',
+      'sanitize_fn' => 'sanitize_location',
+      'pass_data_to_display_fn' => false,
+      'validation_error' => 'Display location must be beginning or end',
     ),
     'headline' => array(
       'default_val' => 'Post Statistics',
       'display_name' => 'Headline Text',
       'display_fn' => 'headline_html',
+      'sanitize_fn' => 'default',
+      'pass_data_to_display_fn' => false,
+      'validation_error' => 'Headline must be valid text',
     ),
+    'word_count' => array(
+      'default_val' => '1',
+      'display_name' => 'Word Count',
+      'display_fn' => 'checkbox_html',
+      'sanitize_fn' => 'sanitize_word_count',
+      'pass_data_to_display_fn' => true,
+      'validation_error' => 'Checkbox values can only be checked or unchecked',
+    ),
+    'character_count' => array(
+      'default_val' => '0',
+      'display_name' => 'Character Count',
+      'display_fn' => 'checkbox_html',
+      'sanitize_fn' => 'sanitize_character_count',
+      'pass_data_to_display_fn' => true,
+      'validation_error' => 'Checkbox values can only be checked or unchecked',
+    ),
+    'read_time' => array(
+      'default_val' => '0',
+      'display_name' => 'Read Time',
+      'display_fn' => 'checkbox_html',
+      'sanitize_fn' => 'sanitize_read_time',
+      'pass_data_to_display_fn' => true,
+      'validation_error' => 'Checkbox values can only be checked or unchecked',
+    )
   );
   function __construct()
   {
     add_action('admin_menu', array($this, 'admin_menu_option'));
     add_action('admin_init', array($this, 'settings'));
+  }
+
+  private function get_setting_error_name($base_name)
+  {
+    $setting_name = $this->get_full_setting_name($base_name);
+    return "$setting_name$this->setting_error_suffix";
   }
 
   private function get_full_setting_name($base_name)
@@ -54,8 +93,10 @@ class Vecktor_TerraOnePlugin
     foreach ($this->setting_base_names as $base_name) {
       list(
         'display_name' => $display_name,
-        'display_fn' => $display_fn,
         'default_val' => $default_val,
+        'display_fn' => $display_fn,
+        'sanitize_fn' => $sanitize_fn,
+        'pass_data_to_display_fn' => $pass_data_to_display_fn,
       ) = $this->setting_name_to_data[$base_name];
       $full_setting_name = $this->get_full_setting_name($base_name);
       add_settings_field(
@@ -64,13 +105,49 @@ class Vecktor_TerraOnePlugin
         array($this, $display_fn),
         $this->setting_slug,
         $this->setting_section_default,
+        $pass_data_to_display_fn ? array($base_name) : array(),
       );
       register_setting(
         $this->setting_group,
         $full_setting_name,
-        array('sanitize_callback' => 'sanitize_text_field', 'default' => $default_val),
+        array('sanitize_callback' => $sanitize_fn == 'default' ? 'sanitize_text_field' : array($this, $sanitize_fn), 'default' => $default_val),
       );
     }
+  }
+
+  function sanitize_location($input)
+  {
+    return $this->sanitize_on_and_off($input, 0);
+  }
+
+  function sanitize_word_count($input)
+  {
+    return $this->sanitize_on_and_off($input, 2, true);
+  }
+
+  function sanitize_character_count($input)
+  {
+    return $this->sanitize_on_and_off($input, 3, true);
+  }
+
+  function sanitize_read_time($input)
+  {
+    return $this->sanitize_on_and_off($input, 4, true);
+  }
+
+  function sanitize_on_and_off($input, $base_name_index, $allow_empty = false)
+  {
+    $base_name = $this->setting_base_names[$base_name_index];
+    $setting_name = $this->get_full_setting_name($base_name);
+    if ($allow_empty && $input == '') return get_option($setting_name);
+    if (!in_array($input, array('0', '1'))) {
+      $error_name = $this->get_setting_error_name($base_name);
+      list('validation_error' => $validation_error) = $this->setting_name_to_data[$base_name];
+      add_settings_error($setting_name, $error_name, $validation_error);
+      return get_option($setting_name);
+    }
+
+    return $input;
   }
 
   function location_html()
@@ -89,6 +166,14 @@ class Vecktor_TerraOnePlugin
     $setting_name = $this->get_full_setting_name($base_name);
   ?>
     <input name="<?php echo $setting_name ?>" value="<? echo $this->safely_get_option($base_name); ?>" />
+  <?php }
+
+  function checkbox_html($args)
+  {
+    $base_name = $args[0];
+    $setting_name = $this->get_full_setting_name($base_name);
+  ?>
+    <input type="checkbox" name="<?php echo $setting_name ?>" value="1" <?php checked(get_option($setting_name), '1') ?> />
   <?php }
 
   function safely_get_option($base_name)
